09e1f4b254ea80667a761c0bc42ba6c9017effe6
Juan <jpcerronex@gmail.com>
2021-09-04 19:14:50 -0300

    added sine wave + change pitch

--- code/main.cpp
+++ code/main.cpp
@@ -2,10 +2,25 @@
 #include <stdint.h>
 #include <Xinput.h>
 #include <stdio.h>
-#include  <xaudio2.h>
+#include <xaudio2.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 static bool running;
 static int xOffset = 0;
+static int cycles = 512;
+static IXAudio2SourceVoice *pSourceVoice;
+
+struct audioBuffer {
+  int wBitsPerSample = 16;
+  int sampleRate = 44100;
+  int seconds = 1;
+  int16_t volume = 1000;
+  int latencySamples = sampleRate / 30;
+  int bufferElements = (wBitsPerSample * sampleRate * seconds * 2) / 8;
+  BYTE *pDataBuffer = new BYTE[(wBitsPerSample * sampleRate * seconds * 2) / 8];
+};
+
 struct backBuffer
 {
   BITMAPINFO info;
@@ -23,7 +38,7 @@ struct Dimension
 };
 
 static backBuffer globalBackBuffer;
-
+static audioBuffer globalAudioBuffer;
 Dimension getWindowDimension(HWND window)
 {
   RECT rect;
@@ -34,71 +49,117 @@ Dimension getWindowDimension(HWND window)
   return d;
 }
 
-HRESULT initSound(){
-    //Sound
+void fillBufferWithSquareWave(BYTE *pDataBuffer, int sampleRate, int bufferElements)
+{
+  int16_t volume = 1000;
+  int16_t *sample = (int16_t *)pDataBuffer;
+  int frecuencyHz = 200;
+  int cycles = (sampleRate / frecuencyHz);
+  int counter = 0;
+  for (int i = 0; i < bufferElements / 8; i++)
+  {
+    // one sample at a time
+    if (counter < cycles)
+    {
+      *(sample) = volume;
+      *(sample + 1) = volume;
+      *(sample + 2) = volume;
+      *(sample + 3) = volume;
+    }
+    else
+    {
+      *(sample) = -volume;
+      *(sample + 1) = -volume;
+      *(sample + 2) = -volume;
+      *(sample + 3) = -volume;
+    }
+    counter++;
+    if (counter == cycles * 2)
+    {
+      counter = 0;
+    }
+    else
+      sample = sample + 4;
+  }
+}
+
+void fillBufferWithSineWave(audioBuffer buffer)
+{
+  XAUDIO2_VOICE_STATE bufferState;
+  int samplesPlayed;
+  if(pSourceVoice){
+    pSourceVoice->GetState(&bufferState);
+    samplesPlayed = bufferState.SamplesPlayed % buffer.sampleRate;
+  }
+  else{
+    samplesPlayed = 0;
+  }
+  /*
+  int16_t *sample = (int16_t *)buffer.pDataBuffer;
+  for (int i = 0; i < buffer.sampleRate; i++)
+  {
+    // one sample at a time
+    float value = i*cycles*2*M_PI/buffer.sampleRate;
+    *(sample) = buffer.volume * sin(value);
+    *(sample + 1) = buffer.volume * sin(value);
+    sample = sample + 2;
+  }*/
+  int16_t *sample = (int16_t *)buffer.pDataBuffer;
+  sample += samplesPlayed*2;
+  for(int i = 0; i < buffer.sampleRate - samplesPlayed;i++){
+    // one sample at a time
+    float value = i*cycles*2*M_PI/buffer.sampleRate;
+    *(sample) = buffer.volume * sin(value);
+    *(sample + 1) = buffer.volume * sin(value);
+    sample = sample + 2;
+  }
+  sample = (int16_t *)buffer.pDataBuffer;
+  for(int i = buffer.sampleRate - samplesPlayed; i < buffer.sampleRate;i++){
+    // one sample at a time
+    float value = i*cycles*2*M_PI/buffer.sampleRate;
+    *(sample) = buffer.volume * sin(value);
+    *(sample + 1) = buffer.volume * sin(value);
+    sample = sample + 2;
+  }
+}
+
+HRESULT initSound()
+{
+  //Sound
   HRESULT hr;
-  hr = CoInitializeEx( nullptr, COINIT_MULTITHREADED );
+  hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
   if (FAILED(hr))
     return hr;
-  IXAudio2* audioObject;
-  if(FAILED(hr = XAudio2Create(&audioObject, 0, XAUDIO2_DEFAULT_PROCESSOR))){
+  IXAudio2 *audioObject;
+  if (FAILED(hr = XAudio2Create(&audioObject, 0, XAUDIO2_DEFAULT_PROCESSOR)))
+  {
     return hr;
   };
   IXAudio2MasteringVoice *masteringVoice;
-  if(FAILED(hr = audioObject->CreateMasteringVoice(&masteringVoice))){
+  if (FAILED(hr = audioObject->CreateMasteringVoice(&masteringVoice)))
+  {
     return hr;
   }
-  int wBitsPerSample = 16;
-  int sampleRate = 44100;
-  int seconds = 1;
-  // CHECK 4th PARAM
-  WAVEFORMATEX wfx = {WAVE_FORMAT_PCM, 2, sampleRate, sampleRate*(2*wBitsPerSample)/8, (2*wBitsPerSample)/8, wBitsPerSample, 0} ;
-  BYTE *pDataBuffer = new BYTE[(wBitsPerSample*sampleRate*seconds*2)/8];
-  int bufferElements = (wBitsPerSample*sampleRate*seconds*2)/8;
-  int16_t volume = 1000;
-  
-  int16_t *sample = (int16_t *)pDataBuffer;
-  int frecuencyHz = 200;
-  int cycles = (sampleRate/frecuencyHz);
-  int counter = 0;
-  for(int i=0; i < bufferElements/8 ;i++){
-    // one sample at a time
-    if(counter < cycles){
-      *(sample)= volume;
-      *(sample+1) = volume;
-      *(sample+2) = volume;
-      *(sample+3) = volume;
-    }
-    else{
-      *(sample) = -volume;
-      *(sample+1) = -volume;
-      *(sample+2) = -volume;
-      *(sample+3) = -volume;
-    }
-    counter++;
-    if (counter == cycles*2){
-      counter = 0;
-    }
-    else 
-    sample = sample+4;
-  }
-  
+
+  WAVEFORMATEX wfx = {WAVE_FORMAT_PCM, 2, globalAudioBuffer.sampleRate, globalAudioBuffer.sampleRate * (2 * globalAudioBuffer.wBitsPerSample) / 8, (2 * globalAudioBuffer.wBitsPerSample) / 8, globalAudioBuffer.wBitsPerSample, 0};
+
+  fillBufferWithSineWave(globalAudioBuffer);
+
   XAUDIO2_BUFFER buffer = {0};
   buffer.Flags = XAUDIO2_END_OF_STREAM;
-  buffer.AudioBytes = bufferElements;
-  buffer.pAudioData = pDataBuffer;
-  buffer.LoopLength = sampleRate;
+  buffer.AudioBytes = globalAudioBuffer.bufferElements;
+  buffer.pAudioData = globalAudioBuffer.pDataBuffer;
+  buffer.LoopLength = globalAudioBuffer.sampleRate;
   buffer.LoopCount = XAUDIO2_LOOP_INFINITE;
-  IXAudio2SourceVoice* pSourceVoice;
-  if( FAILED(hr = audioObject->CreateSourceVoice( &pSourceVoice, (WAVEFORMATEX*)&wfx ) ) ) return hr;
-
-  if( FAILED(hr = pSourceVoice->SubmitSourceBuffer( &buffer ) ) )
+  if (FAILED(hr = audioObject->CreateSourceVoice(&pSourceVoice, (WAVEFORMATEX *)&wfx)))
     return hr;
-  
-  if ( FAILED(hr = pSourceVoice->Start( 0 ) ) )
+
+  if (FAILED(hr = pSourceVoice->SubmitSourceBuffer(&buffer)))
+    return hr;
+
+  if (FAILED(hr = pSourceVoice->Start(0)))
     return hr;
 }
-
 
 void renderGradient(backBuffer buffer, int xOffset)
 {
@@ -135,7 +196,7 @@ void resizeDIBSecion(backBuffer *buffer, int width, int height)
   buffer->info.bmiHeader.biCompression = BI_RGB;
 
   int bitmapMemorySize = buffer->bytesPerPixel * buffer->width * buffer->height;
-  buffer->memory = VirtualAlloc(0, bitmapMemorySize, MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
+  buffer->memory = VirtualAlloc(0, bitmapMemorySize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
   buffer->pitch = buffer->bytesPerPixel * buffer->width;
 }
 
@@ -194,22 +255,31 @@ LRESULT CALLBACK MainWindowCallback(
     bool wasDown = (lParam & (1 << 30)) != 0;
     bool isDown = (lParam & (1 << 31)) != 0;
     bool isAltPressed = (lParam & (1 << 29)) != 0;
-    if(wasDown)
+    if (wasDown)
       OutputDebugStringA("was down\n");
-    if(isDown)
+    if (isDown)
       OutputDebugStringA("is down\n");
-    switch (wParam){
-      case VK_RIGHT:{
-        OutputDebugStringA("RIGHT");
-        xOffset+=50;
-      };
-      case VK_F4:{
-        if(isAltPressed){
-          running = false;
-        }
+    switch (wParam)
+    {
+    case VK_RIGHT:
+    {
+      OutputDebugStringA("RIGHT");
+      xOffset += 50;
+    } break;
+    case 'A':
+    {
+      cycles += 32;
+      fillBufferWithSineWave(globalAudioBuffer);
+
+    } break;
+    case VK_F4:
+    {
+      if (isAltPressed)
+      {
+        running = false;
       }
     }
-  
+    }
   }
   break;
   default:
@@ -227,7 +297,7 @@ int CALLBACK WinMain(
     LPSTR lpCmdLine,
     int nShowCmd)
 {
-  
+
   //Window
   WNDCLASSA windowClass = {};
   resizeDIBSecion(&globalBackBuffer, 800, 800);
@@ -241,10 +311,11 @@ int CALLBACK WinMain(
                                         "Handmade Hero", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
                                         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
                                         0, 0, hInstance, 0);
-    if(SUCCEEDED(initSound())){
+    if (SUCCEEDED(initSound()))
+    {
       OutputDebugStringA("Sound initialized\n");
     }
-    
+
     if (windowHandle)
     {
       running = true;
@@ -260,7 +331,7 @@ int CALLBACK WinMain(
           TranslateMessage(&message);
           DispatchMessage(&message);
         }
-    /*
+        /*
     DWORD dwResult;    
     for (DWORD i=0; i< XUSER_MAX_COUNT; i++ )
     {
