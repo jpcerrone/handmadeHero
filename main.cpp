3e1e37f08df88001cca6b367d50c7bd40b8c75b3
jpcerrone <jpcerronex@gmail.com>
2023-05-04 14:50:50 -0300

    messageBox

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){
+    MessageBox(NULL, "Jodot Begins", "ATENTION", MB_ICONEXCLAMATION);
+    return 0;
+};
