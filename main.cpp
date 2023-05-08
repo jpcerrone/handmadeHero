165dec5eb4ab9ffcf9acecf39a6101462ba1dbd9
jpcerrone <jpcerronex@gmail.com>
2023-05-08 15:16:20 -0300

    exit dialog

--- main.cpp
+++ main.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
 #include <windows.h>
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+bool gameRunning;
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+    PAINTSTRUCT lpPaint;
     LRESULT returnVal = 0;
+    int width = lpPaint.rcPaint.right - lpPaint.rcPaint.left;
+    int height = lpPaint.rcPaint.bottom - lpPaint.rcPaint.top;
     switch (uMsg)
     {
         case WM_CLOSE:
         {
-            std::cout << "WM_CLOSE";
-            
+            if (MessageBox(hWnd, "Sure you want to exit?", "Jodot - Exiting", MB_YESNO) == IDYES){
+                gameRunning = false;
+                DestroyWindow(hWnd);            
+            };
+        } break;
+        case WM_DESTROY:
+        {
+            // ??? Handle as an error
+            DestroyWindow(hWnd);            
         } break;
         case WM_PAINT:
         {
             static DWORD color = BLACKNESS;
             HDC hdc; 
-            PAINTSTRUCT lpPaint;
             hdc = BeginPaint(hWnd, &lpPaint);
             color = (color == BLACKNESS) ? WHITENESS : BLACKNESS;
-            PatBlt(hdc, lpPaint.rcPaint.left, lpPaint.rcPaint.top, lpPaint.rcPaint.right - lpPaint.rcPaint.left, lpPaint.rcPaint.bottom - lpPaint.rcPaint.top, color);
+            PatBlt(hdc, lpPaint.rcPaint.left, lpPaint.rcPaint.top, width, height, color);
             EndPaint(hWnd, &lpPaint);
         } break;
         default:
@@ -28,20 +39,22 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
     return returnVal;
 };
 
+// hInstance: handle to the .exe
+// hPrevInstance: not used since 16bit windows
+// WINAPI: calling convention, tells compiler order of parameters
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){
     WNDCLASS wc = {};
-    
-    //wc.cbSize = sizeof(WNDCLASSEXW);
+    gameRunning = true;
 
     wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.lpfnWndProc = WndProc;
+	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance ? hInstance : GetModuleHandle(nullptr);
 	wc.lpszClassName = "Engine";
 
     if (RegisterClass(&wc)){
         HWND windowHandle = CreateWindowEx(0, wc.lpszClassName, "Jodot Engine", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, hInstance, 0);
         if (windowHandle){
-            while(true){
+            while(gameRunning){
                 MSG message;
                 BOOL messageResult = GetMessage(&message, 0, 0, 0);
                 if (messageResult > 0){
