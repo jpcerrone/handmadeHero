ad0014d816485eafa99384caa2238bd49b0e026d
jpcerrone <jpcerronex@gmail.com>
2023-05-15 17:25:56 -0300

    refactor gradient rendering

--- main.cpp
+++ main.cpp
@@ -5,20 +5,23 @@ struct Dimension{
     int width, height;
 };
 
+int xOffset = 0;
 static bool gameRunning;
 
-static Dimension clientWindowDimensions;
-void* bitsMapMemory;
-BITMAPINFO bmInfo;
-int xOffset = 0;
+struct Bitmap{
+    void* memory;
+    BITMAPINFO info;
+    Dimension dimensions;
+};
 
-static Dimension bitmapDimensions;
+static Dimension clientWindowDimensions;
+static Bitmap globalBitmap;
 
 void renderGradient(int xOffset){
     // pixel = 4B = 32b
-    uint32_t *pixel = (uint32_t*) bitsMapMemory;
-    for (int y=0; y < bitmapDimensions.height;y++){
-        for (int x=0; x < bitmapDimensions.width;x++){
+    uint32_t *pixel = (uint32_t*) globalBitmap.memory;
+    for (int y=0; y < globalBitmap.dimensions.height;y++){
+        for (int x=0; x < globalBitmap.dimensions.width;x++){
             uint8_t red = 0x00;
             uint8_t green = 0;
             uint8_t blue = x + xOffset;
@@ -30,68 +33,71 @@ void renderGradient(int xOffset){
 }
 
 void resizeDibSection(int width, int height){
-    if (bitsMapMemory){
-        VirtualFree(bitsMapMemory, 0, MEM_RELEASE);
+    if (globalBitmap.memory){
+        VirtualFree(globalBitmap.memory, 0, MEM_RELEASE);
     }
 
-    bitmapDimensions = {width, height};
+    globalBitmap.dimensions = {width, height};
 
     BITMAPINFOHEADER bmInfoHeader = {};
     bmInfoHeader.biSize = sizeof(bmInfoHeader);
     bmInfoHeader.biCompression = BI_RGB;
     bmInfoHeader.biWidth = width;
-    bmInfoHeader.biHeight = -height;
+    bmInfoHeader.biHeight = -height; // Negative means it'll be filled top-down
     bmInfoHeader.biPlanes = 1; // MSDN sais it must be set to 1, legacy reasons
     bmInfoHeader.biBitCount = 32; // R+G+B+padding each 8bits
-    bmInfo.bmiHeader = bmInfoHeader;
+    globalBitmap.info.bmiHeader = bmInfoHeader;
 
-    bitsMapMemory = VirtualAlloc(0, width*height*4, MEM_COMMIT, PAGE_READWRITE);
+    globalBitmap.memory = VirtualAlloc(0, width*height*4, MEM_COMMIT, PAGE_READWRITE);
 }
 
 void updateWindow(HDC deviceContext, int srcX, int srcY, int srcWidth, int srcHeight, int windowWidth, int windowHeight, void* bitMapMemory, BITMAPINFO bitmapInfo){
-    StretchDIBits(deviceContext, 0, 0, srcWidth, srcHeight, 0, 0, windowWidth, windowHeight, bitMapMemory, &bitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+    StretchDIBits(deviceContext, 0, 0, windowWidth, windowHeight, 0, 0, srcWidth, srcHeight, bitMapMemory, &bitmapInfo, DIB_RGB_COLORS, SRCCOPY);
 }
 
-LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+Dimension getWindowDimension(HWND windowHandle){
+    RECT clientRect;
+    GetClientRect(windowHandle, &clientRect);
+    int width = clientRect.right - clientRect.left;
+    int height = clientRect.bottom - clientRect.top;
+    Dimension retDimension = {width, height};
+    return retDimension;
+}
+
+LRESULT CALLBACK WindowProc(HWND windowHandle, UINT uMsg, WPARAM wParam, LPARAM lParam){
     LRESULT returnVal = 0;
     switch (uMsg)
     {
         case WM_SIZE:
         {
-            RECT clientRect;
-            GetClientRect(hWnd, &clientRect);
-            int width = clientRect.right - clientRect.left;
-            int height = clientRect.bottom - clientRect.top;
-            clientWindowDimensions = {width, height};
-            resizeDibSection(width, height);
+            clientWindowDimensions = getWindowDimension(windowHandle);
         } break;
         case WM_CLOSE:
         {
             #ifdef _DEBUG
                 std::cout << "DEBUG: exit popup" << std::endl;
             #endif 
-            if (MessageBox(hWnd, "Sure you want to exit?", "Jodot - Exiting", MB_YESNO) == IDYES){
+            if (MessageBox(windowHandle, "Sure you want to exit?", "Jodot - Exiting", MB_YESNO) == IDYES){
                 gameRunning = false;
-                DestroyWindow(hWnd);            
+                DestroyWindow(windowHandle);            
             };
         } break;
         case WM_DESTROY:
         {
             // ??? Handle as an error
-            DestroyWindow(hWnd);            
+            DestroyWindow(windowHandle);            
         } break;
         case WM_PAINT:
         {   
             PAINTSTRUCT paintStruct;
-            HDC hdc = BeginPaint(hWnd, &paintStruct);
-            xOffset++;
+            HDC hdc = BeginPaint(windowHandle, &paintStruct);
             renderGradient(xOffset);
-            updateWindow(hdc, 0, 0, bitmapDimensions.width, bitmapDimensions.height, clientWindowDimensions.width, clientWindowDimensions.height,  bitsMapMemory, bmInfo);
-            EndPaint(hWnd, &paintStruct);      
+            updateWindow(hdc, 0, 0, globalBitmap.dimensions.width, globalBitmap.dimensions.height, clientWindowDimensions.width, clientWindowDimensions.height,  globalBitmap.memory, globalBitmap.info);
+            EndPaint(windowHandle, &paintStruct);      
         } break;
         default:
         {
-            returnVal = DefWindowProc(hWnd, uMsg, wParam, lParam);
+            returnVal = DefWindowProc(windowHandle, uMsg, wParam, lParam);
         } break;
     }
     return returnVal;
@@ -109,6 +115,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.hInstance = hInstance ? hInstance : GetModuleHandle(nullptr);
 	wc.lpszClassName = "Engine";
 
+    globalBitmap.dimensions = {1920,1080};
+    resizeDibSection(globalBitmap.dimensions.width, globalBitmap.dimensions.height);
+
     if (RegisterClass(&wc)){
         HWND windowHandle = CreateWindowEx(0, wc.lpszClassName, "Jodot Engine", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, hInstance, 0);
         if (windowHandle){
@@ -125,7 +134,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
                 renderGradient(xOffset);
                 xOffset++;
                 HDC windowDeviceContext = GetDC(windowHandle);
-                updateWindow(windowDeviceContext, 0, 0, bitmapDimensions.width, bitmapDimensions.height, clientWindowDimensions.width, clientWindowDimensions.height,  bitsMapMemory, bmInfo);
+                updateWindow(windowDeviceContext, 0, 0, globalBitmap.dimensions.width, globalBitmap.dimensions.height, clientWindowDimensions.width, clientWindowDimensions.height,  globalBitmap.memory, globalBitmap.info);
                 ReleaseDC(windowHandle, windowDeviceContext);
             }
         }
